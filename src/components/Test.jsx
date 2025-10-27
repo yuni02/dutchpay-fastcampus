78a90cd70ba1c65a0d69acff661ae0c54c4f8623
Yunkyeong <yunkyeong@ccmedia.co.kr>
2025-10-27 12:41:06 +0900

    chore: bootstrap과 React-router-dom library 추가

---
+++ src/components/Test.jsx
@@ -0,0 +1,14 @@
+import {Col, Container, Row} from "react-bootstrap";
+
+export const Test = () => (
+    <Container>
+      <Row>
+        <Col xs={6}>
+          Column1
+        </Col>
+        <Col xs={6}>
+          Column2
+        </Col>
+      </Row>
+    </Container>
+)
